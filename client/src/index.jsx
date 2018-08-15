8abc3eafdee9e9d69111a312177fa1888e2fae8d
Kausar Khizra <khizra6@gmail.com>
2018-08-15 13:48:30 -0700

    added karma coverage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,3 +7,6 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// Do NOT include this file in karma.conf.js for test coverage BECAUSE it will give target error. This error occurs
+// because of the ReactDOM render line. So either exclude this file OR write your pattern right.
--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,37 @@
+// Karma configuration
+// Generated on Tue Aug 14 2018 10:44:03 GMT-0700 (PDT)
+
+
+// var path = require('path');
+// var TEST_DIR = path.join(__dirname, '/test/clientSpecs');
+// var webpackConfig = require('./webpack.config.js');
+// webpackConfig.entry = undefined;
+
+// module.exports = function(config) {
+//   config.set({
+//     basePath: '',
+//     frameworks: ['mocha', 'chai'],
+//     files: [
+//       { pattern: 'tests/**/*.js', watching: true }
+//     ],
+//     exclude: [
+//       'tests/**/*.swp'
+//     ],
+//     preprocessors: {
+//         'tests/**/*.js': ['webpack']
+//     },
+//     webpack: webpackConfig,
+//     reporters: ['progress'],
+//     port: 9876,
+//     colors: true,
+//     logLevel: config.LOG_DEBUG,
+//     autoWatch: true,
+//     browsers: ['Chrome'],
+//     singleRun: false,
+//     concurrency: Infinity
+//   })
+// }
+
 
 var path = require('path');
 var TEST_DIR = path.join(__dirname, '/test/clientSpecs');
@@ -6,19 +40,29 @@ webpackConfig.entry = undefined;
 
 module.exports = function(config) {
   config.set({
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+
+    reporters: ['progress', 'coverage'],
+
     basePath: '',
     frameworks: ['mocha', 'chai'],
     files: [
-      { pattern: 'tests/**/*.js', watching: true }
+      'client/src/components/*.jsx',
+      { pattern: 'tests/**/*.js'}
     ],
     exclude: [
       'tests/**/*.swp'
     ],
     preprocessors: {
-        'tests/**/*.js': ['webpack']
+        'client/src/components/*.jsx': ['webpack', 'coverage'],
+        'tests/**/*.js': ['webpack'] // DO NOT ADD test in test coverage
     },
     webpack: webpackConfig,
-    reporters: ['progress'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_DEBUG,
@@ -28,3 +72,12 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 }
+
+
+// reference - https://www.npmjs.com/package/karma-coverage
+
+
+
+
+
+
