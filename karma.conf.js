03e7abc0c506044306bc0bf9930b964d8846ebc3
Kausar Khizra <khizra6@gmail.com>
2018-08-15 14:12:12 -0700

    cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,5 @@
-// Karma configuration
-// Generated on Tue Aug 14 2018 10:44:03 GMT-0700 (PDT)
-
-
-// var path = require('path');
-// var TEST_DIR = path.join(__dirname, '/test/clientSpecs');
-// var webpackConfig = require('./webpack.config.js');
-// webpackConfig.entry = undefined;
-
-// module.exports = function(config) {
-//   config.set({
-//     basePath: '',
-//     frameworks: ['mocha', 'chai'],
-//     files: [
-//       { pattern: 'tests/**/*.js', watching: true }
-//     ],
-//     exclude: [
-//       'tests/**/*.swp'
-//     ],
-//     preprocessors: {
-//         'tests/**/*.js': ['webpack']
-//     },
-//     webpack: webpackConfig,
-//     reporters: ['progress'],
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_DEBUG,
-//     autoWatch: true,
-//     browsers: ['Chrome'],
-//     singleRun: false,
-//     concurrency: Infinity
-//   })
-// }
-
 
 var path = require('path');
-var TEST_DIR = path.join(__dirname, '/test/clientSpecs');
 var webpackConfig = require('./webpack.config.js');
 webpackConfig.entry = undefined;
 
